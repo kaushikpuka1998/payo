35c320bddf06fb9b1d4fd2b7d336b5c47b3e0531
kasuhikpuka1998 <kaushikghosh199832@gmail.com>
2022-02-07 22:04:16 +0530

    Dynamic Input

--- app/src/main/java/com/kgstrivers/payoneer/Activities/SecondPageActivity.java
+++ app/src/main/java/com/kgstrivers/payoneer/Activities/SecondPageActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.kgstrivers.payoneer.Models.Applicable;
+import com.kgstrivers.payoneer.Models.InputElement;
 import com.kgstrivers.payoneer.R;
 
 import java.util.List;
@@ -18,6 +20,7 @@ public class SecondPageActivity extends AppCompatActivity {
 
     EditText et1,et2,et3,et4,et5;
     String Inputelemets[]= {"number","expiryMonth","expiryYear","verificationCode","holderName","bic","iban"};
+    String Inputelemetstype[]= {"number","integer","integer","integer","string","string","string"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,50 +32,57 @@ public class SecondPageActivity extends AppCompatActivity {
 
         if(id == 0 || id == 1 || id == 2 || id == 4 || id == 5 ||id == 13||id == 14||id == 15 ||id == 16)
         {
-            enableandsethint(et1,Inputelemets[0]);
-            enableandsethint(et2,Inputelemets[1]);
-            enableandsethint(et3,Inputelemets[2]);
-            enableandsethint(et4,Inputelemets[3]);
-            enableandsethint(et5,Inputelemets[4]);
+            enableandsethint(et1,Inputelemets[0],Inputelemetstype[0]);
+            enableandsethint(et2,Inputelemets[1],Inputelemetstype[1]);
+            enableandsethint(et3,Inputelemets[2],Inputelemetstype[2]);
+            enableandsethint(et4,Inputelemets[3],Inputelemetstype[3]);
+            enableandsethint(et5,Inputelemets[4],Inputelemetstype[4]);
         }
         else if(id == 3)
         {
-            enableandsethint(et1,Inputelemets[5]);
+            enableandsethint(et1,Inputelemets[5],Inputelemetstype[5]);
         }
         else if(id == 6)
         {
-            enableandsethint(et1,Inputelemets[0]);
-            enableandsethint(et2,Inputelemets[2]);
-            enableandsethint(et3,Inputelemets[1]);
+            enableandsethint(et1,Inputelemets[0],Inputelemetstype[0]);
+            enableandsethint(et2,Inputelemets[2],Inputelemetstype[2]);
+            enableandsethint(et3,Inputelemets[1],Inputelemetstype[1]);
         }
         else if(id == 9)
         {
-            enableandsethint(et1,Inputelemets[4]);
-            enableandsethint(et2,Inputelemets[0]);
-            enableandsethint(et3,Inputelemets[2]);
-            enableandsethint(et4,Inputelemets[1]);
-            enableandsethint(et5,Inputelemets[3]);
+            enableandsethint(et1,Inputelemets[4],Inputelemetstype[4]);
+            enableandsethint(et2,Inputelemets[0],Inputelemetstype[0]);
+            enableandsethint(et3,Inputelemets[2],Inputelemetstype[2]);
+            enableandsethint(et4,Inputelemets[1],Inputelemetstype[1]);
+            enableandsethint(et5,Inputelemets[3],Inputelemetstype[3]);
         }
         else if(id == 11)
         {
-            enableandsethint(et1,Inputelemets[4]);
-            enableandsethint(et2,Inputelemets[6]);
-            enableandsethint(et3,Inputelemets[5]);
-        }
-
-        else if(id == 11)
-        {
-            enableandsethint(et1,Inputelemets[4]);
-            enableandsethint(et2,Inputelemets[6]);
-            enableandsethint(et3,Inputelemets[5]);
+            enableandsethint(et1,Inputelemets[4],Inputelemetstype[4]);
+            enableandsethint(et2,Inputelemets[6],Inputelemetstype[6]);
+            enableandsethint(et3,Inputelemets[5],Inputelemetstype[5]);
         }
         Toast.makeText(this,applicable.getLabel()+id , Toast.LENGTH_SHORT).show();
 
 
     }
-    private void enableandsethint(EditText et,String hint)
+    private void enableandsethint(EditText et,String hint,String inputtype)
     {
         et.setVisibility(View.VISIBLE);
+        if(inputtype == "string")
+        {
+            et.setInputType(InputType.TYPE_CLASS_TEXT);
+        }
+        else
+        {
+            et.setInputType(InputType.TYPE_CLASS_NUMBER);
+        }
+
+        if(hint == "expiryYear" || hint == "expiryMonth" )
+        {
+            et.setInputType(InputType.TYPE_CLASS_DATETIME);
+        }
+
         et.setHint(hint);
     }
 
